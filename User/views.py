250241421d3970f932da9adc3722b7485927e7e6
sauravbiswas <saru.sreyo@gmail.com>
2016-10-25 23:23:33 +0530

    New Commit

--- User/views.py
+++ User/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def index(request):
     if request.method == 'GET':
         return render(request,'User/index.html',{})
