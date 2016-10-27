aebee4e5aa665db564332e8b706cc7afa8ef78c4
sauravbiswas <saru.sreyo@gmail.com>
2016-10-27 07:39:20 +0530

    First

--- User/models.py
+++ User/models.py
@@ -2,15 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
-class FaseClient(models.Model):
-    fase_user = models.OneToOneField(User, on_delete=models.CASCADE)
-    path = models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.user.get_full_name()
-
-
-class FaseMerchant(models.Model):
-    pass
