a8d6a4c418bd5bf1d3f80f34ec6944a9f97450c6
sauravbiswas <saru.sreyo@gmail.com>
2016-11-12 01:07:00 +0530

    Something

--- Company/models.py
+++ Company/models.py
@@ -15,7 +15,3 @@ class Stock(models.Model):
         return self.name + ' ' + self.ticker
 
 
-class CitrusResponse(models.Model):
-    response_String = models.TextField()
-    data_string = models.TextField()
-    transaction_id = models.CharField(max_length=255, unique=True)
