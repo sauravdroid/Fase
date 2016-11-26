0f549803f2dd0d77eb0c8617f6d00a371fb8bd67
sauravbiswas <saru.sreyo@gmail.com>
2016-11-26 11:12:59 +0530

    Add CreatedApps model

--- User/models.py
+++ User/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+from django.utils import timezone
 
 
 class StockNew(models.Model):
@@ -21,28 +26,41 @@ class CitrusResponse(models.Model):
     def __str__(self):
         return self.transaction_id + " | " + self.response_string
 
+
 class Seller(models.Model):
-    seller=models.OneToOneField(User,on_delete=models.CASCADE)
-    company_name=models.CharField(max_length=255)
-    adress=models.TextField()
-    city=models.CharField(max_length=255,default="none")
-    state=models.CharField(max_length=255,default="none")
-    phone_no=models.CharField(max_length=255,default="none",unique=True)
-    pincode=models.IntegerField()
+    seller = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=255)
+    adress = models.TextField()
+    city = models.CharField(max_length=255, default="none")
+    state = models.CharField(max_length=255, default="none")
+    phone_no = models.CharField(max_length=255, default="none", unique=True)
+    pincode = models.IntegerField()
 
     def __str__(self):
         return self.seller.username
 
 
 class FavoriteShop(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    fseller=models.ForeignKey(Seller,on_delete=models.CASCADE)
-    tag=models.CharField(unique=True,max_length=510)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fseller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    tag = models.CharField(unique=True, max_length=510)
 
     def __str__(self):
-        return self.tag+str(self.id)
+        return self.tag + str(self.id)
 
 
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
+class CreatedApps(models.Model):
+    app_name = models.CharField(max_length=100, unique=True)
+    app_secret_key = models.CharField(max_length=100, unique=True)
+    app_encrypted_key = models.CharField(max_length=512, unique=True)
+    created_at = models.DateTimeField(default=timezone.now())
+    description = models.TextField(default="A simple android app for fase")
 
+    def __str__(self):
+        return self.app_name
